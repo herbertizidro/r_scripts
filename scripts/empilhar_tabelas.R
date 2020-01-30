a51ed1e4b9e62a6ac94387a89893d0a60e4a9d89
Herbert Izidro <herbertsouza80@yahoo.com>
2020-01-30 09:37:38 -0300

    Update empilhar_tabelas.R

--- scripts/empilhar_tabelas.R
+++ scripts/empilhar_tabelas.R
@@ -3,5 +3,5 @@ library(tidyverse)
 
 df1 <- read.csv2(file = "janeiro-2018.csv")
 df2 <- read.csv2(file = "fevereiro-2018.csv")
-df3 = smartbind(df1, df2) #junta verticalmente duas tabelas
+df3 = smartbind(df1, df2) #junta verticalmente duas ou mais tabelas
 write.csv(df3, file = "jan-fev-2018.csv")
