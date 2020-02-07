e862af339b45b3384e7799f7670935660119dbbb
Herbert Izidro <herbertsouza80@yahoo.com>
2020-02-07 14:59:21 -0300

    Create left_join.R

---
+++ scripts/left_join.R
@@ -0,0 +1,10 @@
+library(xlsx)
+library(dplyr)
+
+dados_cadastrais = read.xlsx("dados_cadastrais.xlsx", 1)
+exibidores = read.xlsx("lista_exibidores.xls", 1)
+names(dados_cadastrais)[3] = "REGISTRO"
+exibidores = exibidores[, 1:6]
+dados_cadastrais = dados_cadastrais[,c(3, 17, 21)]
+output = left_join(exibidores, dados_cadastrais, by="REGISTRO")
+write.xlsx(output, "exibidores_com_email_e_uf.xlsx")
