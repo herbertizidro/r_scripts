8d04c4e1ee2c502a0e5528a75214bd5d114be112
Herbert Izidro <herbertsouza80@yahoo.com>
2020-02-12 23:31:52 -0300

    Update left_join.R

--- scripts/left_join.R
+++ scripts/left_join.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 dados_cadastrais = read.xlsx("dados_cadastrais.xlsx", 1)
 exibidores = read.xlsx("lista_exibidores.xls", 1)
-names(dados_cadastrais)[3] = "REGISTRO"
+names(dados_cadastrais)[3] = "REGISTRO" #porque exibidores tem "REGISTRO"
 exibidores = exibidores[, 1:6]
 dados_cadastrais = dados_cadastrais[,c(3, 17, 21)]
 output = left_join(exibidores, dados_cadastrais, by="REGISTRO")
